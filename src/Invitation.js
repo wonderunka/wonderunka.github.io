e47074a2b8c9f8f6b33465be27d04de225fdbd9b
SERHAT YILMAZ <3996158+serhaty@users.noreply.github.com>
2024-03-24 22:53:58 -0700

    updated styles for mobile screen

--- src/Invitation.js
+++ src/Invitation.js
@@ -69,7 +69,7 @@ const InvitationPage = ({ weddingType, language, locales }) => {
         <div className="text-container subtitle2 margin-top2 uper-case margin-bottom2">
           <div className="margin-top1 margin-bottom3">{locales[language].invitation.receptionToFollow}</div>
         </div>
-        <div className="image-container-bottom" style={{ display: 'flex', alignItems: 'flex-start', justifyContent: 'center' }}>
+        <div className="image-container-bottom">
           <img
             src={design}
             alt="design-bottom"
@@ -131,14 +131,15 @@ const InvitationPage = ({ weddingType, language, locales }) => {
           }
           <div className="text-container subtitle2 margin-top2 uper-case margin-bottom2">
             <div className="margin-top1 margin-bottom3">{locales[language].invitation.receptionToFollow}</div>
+            <div className="image-container-bottom">
+              <img
+                src={design}
+                alt="design-bottom"
+                style={{ transform: 'scale(0.8) rotate(180deg)', width: '1200px', height: '250px' }}
+              />
+            </div>
           </div>
-          <div className="image-container-bottom" style={{ display: 'flex', alignItems: 'flex-start', justifyContent: 'center' }}>
-            <img
-              src={design}
-              alt="design-bottom"
-              style={{ transform: 'scale(0.8) rotate(180deg)', width: '1200px', height: '250px' }}
-            />
-          </div>
+
         </div>
       )
   );
